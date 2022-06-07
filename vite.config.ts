bcbb5d0dde2462fc236dc6a6e4700f4d9310eda2
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-06-07 13:50:35 +0200

    r Oppdaterer navn i test-configen

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default ({ command }: ConfigEnv): UserConfigExport => ({
     },
   },
   test: {
-    global: true,
+    globals: true,
     environment: "jsdom",
     deps: {
       inline: ["@testing-library/user-event"],
