cb4ae9c6ca48bbc5003cae74258b29a44d0d8164
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-06-22 15:37:11 +0200

    Legger til komponenter som ikke ble med da koden ble kopiert fra Veientilarbeid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Rad from "./innhold/rad";
 import ReaktiveringKort from "./komponenter/reaktivering/reaktivering-kort";
-import "@navikt/ds-css";
-import "./App.css";
 import KrrMelding from "./komponenter/krr-melding/krr-melding";
 import StatusTittel from "./komponenter/registrert/status-tittel";
 import OnboardingStandard from "./komponenter/onboarding-standard/onboarding-standard";
@@ -14,6 +12,10 @@ import Egenvurdering from "./komponenter/egenvurdering/egenvurdering";
 import Motestotte from "./komponenter/motestotte/motestotte";
 import InnholdMetrics from "./innhold/innhold-metrics";
 import InViewport from "./komponenter/in-viewport/in-viewport";
+import GenerelleFliser from "./komponenter/dittnav/generelle-fliser";
+import AapRad from "./komponenter/aap/aap";
+import "@navikt/ds-css";
+import "./App.css";
 
 const App: React.FC<{}> = () => {
   // const { data } = useQuery(apiUrl, fetcher);
@@ -33,7 +35,11 @@ const App: React.FC<{}> = () => {
         <Egenvurdering />
         <Motestotte />
         <AktivitetDialogMeldekort />
+        <GenerelleFliser />
       </Rad>
+
+      <AapRad />
+      <InViewport loggTekst="Bunnen av veien til arbeid i viewport" />
     </div>
   );
 };
